69bc6ea098e55e2530fe20e7f7469ec38a3c31c5
wf858 <wf8581@gmail.com>
2020-06-05 21:43:39 -0500

    Fixed static address

--- backend/server.js
+++ backend/server.js
@@ -87,7 +87,7 @@ async function addAnalysisLoop() {
 
 // check for Heroku
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("../../build"));
+    app.use(express.static("client/build"));
     
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
